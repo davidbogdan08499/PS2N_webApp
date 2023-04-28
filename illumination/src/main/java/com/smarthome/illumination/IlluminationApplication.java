0be6e0ce97d429d8fc4471ca82e85a928234f17c
davidbogdan08499 <davidbogdan970@gmail.com>
2023-04-28 16:24:06 +0300

    Initial commit

---
+++ illumination/src/main/java/com/smarthome/illumination/IlluminationApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.illumination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlluminationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlluminationApplication.class, args);
+	}
+
+}
---
+++ illumination/src/test/java/com/smarthome/illumination/IlluminationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.illumination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlluminationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
